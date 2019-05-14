05f33ac571db49a50914ca0f67fe7ee903054f17
Citach <l.acheron01@live.fr>
2019-05-14 10:03:33 +0200

    lll

--- src/com/adaming/run/Run.java
+++ src/com/adaming/run/Run.java
@@ -22,14 +22,14 @@ public class Run {
 //		ITacheService tacheService = (ITacheService) context.getBean("tacheService");
 //		IAffaireService affaireService = (IAffaireService) context.getBean("affaireFactureService");
 //		IPhaseService commercialService = (IPhaseService) context.getBean("phaseService");
-//		ITribunalService tribunalService = (ITribunalService) context.getBean("tribunalService");
+		ITribunalService tribunalService = (ITribunalService) context.getBean("tribunalService");
 		
-//		Tribunal tribu1= new Tribunal("Rue Gambetta",52535355535l,425532424l,"milo");
-//		Tribunal tribu2= new Tribunal("Gtta",56854626l,775225885l,"dijon");
+	Tribunal tribu1= new Tribunal("Rue Gambetta",52535355535l,425532424l,"milo");
+		Tribunal tribu2= new Tribunal("Gtta",56854626l,775225885l,"dijon");
 //		Tribunal tribu3= new Tribunal("Rtta",55777577576l,425886895l,"lorraine");
 //		Tribunal tribu4= new Tribunal("etyilo",52522526l,75754557l,"indre");
-//		tribunalService.create(tribu1);
-//		tribunalService.create(tribu4);
+		tribunalService.create(tribu1);
+	tribunalService.create(tribu2);
 //		tribunalService.create(tribu3);
 //		tribunalService.create(tribu2);
 	}
